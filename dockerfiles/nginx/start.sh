d28dd20b915da964ab35def4067bea69980d93d9
velosoo00 <gaabrielvelooso@gmail.com>
2024-09-26 03:10:28 -0300

    Alter start

--- dockerfiles/nginx/start.sh
+++ dockerfiles/nginx/start.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# Waiting for the database to be ready
-if [ "$DB_CONNECTION" = "mysql" ]; then
-    echo "Waiting for MySQL..."
-    while ! nc -z mysql 3306; do
-        sleep 1
-    done
-    echo "MySQL is ready. Continue..."
-elif [ "$DB_CONNECTION" = "pgsql" ]; then
-    echo "Waiting for PostgreSQL..."
-    while ! nc -z postgres 5432; do
-        sleep 1
-    done
-    echo "PostgreSQL is ready. Continue..."
-fi
-
 echo "Install Composer dependencies..."
 composer install --optimize-autoloader
 
