087a71b91f9aadcb424e4b5f20d37e4ae66dc9ad
velosoo00 <gaabrielvelooso@gmail.com>
2024-09-26 02:53:09 -0300

    Final

--- dockerfiles/nginx/start.sh
+++ dockerfiles/nginx/start.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
-FLAG_FILE="/var/www/.migrations_done"
+# Waiting for the database to be ready
+if [ "$DB_CONNECTION" = "mysql" ]; then
+    echo "Waiting for MySQL..."
+    while ! nc -z mysql 3306; do
+        sleep 1
+    done
+    echo "MySQL is ready. Continue..."
+elif [ "$DB_CONNECTION" = "pgsql" ]; then
+    echo "Waiting for PostgreSQL..."
+    while ! nc -z postgres 5432; do
+        sleep 1
+    done
+    echo "PostgreSQL is ready. Continue..."
+fi
 
 echo "Install Composer dependencies..."
 composer install --optimize-autoloader
 
-# Check if migrations have already been performed
-if [ ! -f "$FLAG_FILE" ]; then
-    echo "Run migrations..."
-    php artisan migrate --force
+echo "Run migrations..."
+php artisan migrate --force
 
-    echo "Create storage link..."
-    php artisan storage:link
-
-    # create the flag file
-    touch "$FLAG_FILE"
-    echo "Migrations and seeds executed successfully!"
-else
-    echo "Migrations have already been executed previously. Skipping execution."
-fi
+echo "Create storage link..."
+php artisan storage:link
 
 # Start PHP-FPM
 php-fpm &
